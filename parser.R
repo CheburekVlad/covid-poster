c6f7e94146915a1c605c8dcb6e121644e4dc66b1
CheburekVlad <shatatel.trub@gmail.com>
2024-01-10 02:49:28 +0100

    upd: parser.R to name data as day

--- parser.R
+++ parser.R
@@ -2,6 +2,7 @@ path <<- "/home/cheburek/Desktop/csse_covid_19_daily_reports"
 setwd(path)
 library(dplyr)
 
+days = tools::file_path_sans_ext(list.files(path, pattern = "\\.csv")[61:1143])
 
 X = function(file){
   Data <- read.csv(file)
@@ -19,10 +20,12 @@ X = function(file){
 
 read = function(path){
   files = list.files(path,pattern= "\\.csv", full.names = TRUE)[61:1143]
+  days = tools::file_path_sans_ext(list.files(path, pattern = "\\.csv")[61:1143])
   dat = lapply(files,X)
+  names(dat) <- days
   return(dat)
 }
   
-
+days = tools::file_path_sans_ext(list.files(path, pattern = "\\.csv")[61:1143])
 l = read(path)
 
